5eb282425bb3f212d421548ba22c67c07174966a
김광현 <khkim@crontiers.com>
2019-04-30 18:28:34 +0900

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  isAuthenticated = false;
+  constructor(
+    private auth : AuthService
+  ){
+    this.auth.onAuthorized.subscribe(
+      () => {
+        this.isAuthenticated = true;
+      }
+    )
+  }
   title = 'last2';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {NgbPaginationModule, NgbAlertModule} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { ResisterComponent } from './resister/resister.component';
+import { HttpClient } from '@angular/common/http';
+import { MemberlistComponent } from './memberlist/memberlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ResisterComponent,
+    MemberlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbPaginationModule,
+    NgbAlertModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path:'memberlist',
+        component:MemberlistComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
